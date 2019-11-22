d09fcc65d003d5cb213129969ee9182808fff665
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-11-22 15:36:28 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './config/StatusBarConfig';
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+
+const Root = () => <Routes />;
+
+export default Root;
---
+++ src/styles/colors.js
@@ -0,0 +1,15 @@
+export default {
+  background: '#222',
+  backgroundDarker: '#111',
+
+  primary: '#3b9eff',
+
+  white: '#FFF',
+  lighter: '#B9BBBE',
+  light: '#999',
+
+  inputBackground: 'rgba(0, 0, 0, 0.1)',
+
+  darkTransparent: 'rgba(0, 0, 0, 0.2)',
+  darkerTransparent: 'rgba(0, 0, 0, 0.7)',
+};
