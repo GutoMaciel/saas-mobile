cb72aa7eeb015c731e66dd097760e559e3747c64
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-11-26 14:58:47 -0300

    Edit member and permissions

--- src/components/Members/index.js
+++ src/components/Members/index.js
@@ -9,12 +9,15 @@ import { bindActionCreators } from 'redux';
 import MembersActions from '../../store/ducks/members';
 
 import InviteMember from '../InviteMember';
+import RoleUpdater from '../RoleUpdater';
 
 import styles from './styles';
 
 class Members extends Component {
   state = {
     isInviteModalOpen: false,
+    isRoleModalOpen: false,
+    memberEdit: null,
   };
 
   componentDidMount() {
@@ -31,9 +34,17 @@ class Members extends Component {
     this.setState({ isInviteModalOpen: false });
   };
 
+  toggleRoleModalOpen = member => {
+    this.setState({ isRoleModalOpen: true, memberEdit: member });
+  };
+
+  toggleRoleModalClosed = () => {
+    this.setState({ isRoleModalOpen: false, memberEdit: null });
+  };
+
   render() {
     const { members } = this.props;
-    const { isInviteModalOpen } = this.state;
+    const { isInviteModalOpen, isRoleModalOpen, memberEdit } = this.state;
 
     return (
       <View style={styles.container}>
@@ -48,7 +59,7 @@ class Members extends Component {
               <Text style={styles.memberName}>{item.user.name}</Text>
               <TouchableOpacity
                 hitSlop={{ top: 5, bottom: 5, left: 5, right: 5 }}
-                onPress={() => {}}
+                onPress={() => this.toggleRoleModalOpen(item)}
               >
                 <Icon name="settings" size={20} color="#999" />
               </TouchableOpacity>
@@ -63,6 +74,14 @@ class Members extends Component {
             </TouchableOpacity>
           )}
         />
+
+        {memberEdit && (
+          <RoleUpdater
+            visible={isRoleModalOpen}
+            onRequestClose={this.toggleRoleModalClosed}
+            member={memberEdit}
+          />
+        )}
 
         <InviteMember
           visible={isInviteModalOpen}
